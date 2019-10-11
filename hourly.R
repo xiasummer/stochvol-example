a17151275a49bad0313bee317130a1727606683f
Kidae Kim <robbberrrtkim@gmail.com>
2019-10-11 12:22:40 +0900

    Update hourly.R

--- hourly.R
+++ hourly.R
@@ -45,3 +45,28 @@ predict_stochvol <- function(train_size=150, predict_length){
 
 pred <- predict_stochvol(predict_length=100)
 pred_time <- data$time[152:251]
+pred_time <- c(pred_time[1:87], as.POSIXct("2019-09-21 05:00:00"), pred_time[88:100])
+
+# This results in plot2.pdf. 
+prediction_df <- data.frame(time=pred_time, 
+                            q1=c(pred[1:87, 1, "y"], NA, pred[88:100, 1, "y"]), 
+                            q2=c(pred[1:87, 2, "y"], NA, pred[88:100, 2, "y"]),
+                            q3=c(pred[1:87, 3, "y"], NA, pred[88:100, 3, "y"]),
+                            return=c(return[151:237], NA, return[238:250]))
+
+  
+ggplot(data=prediction_df) + 
+  geom_line(aes(x=time, y=return, col="return")) + 
+  geom_line(aes(x=time, y=q1, col="interval")) + 
+  geom_line(aes(x=time, y=q3, col="interval")) +  
+  scale_colour_manual("", 
+                      values=c("interval"="red", "return"="darkgreen"),
+                      labels=c("98% posterior interval of one-hour ahead forecast", 
+                               "actual return")) + 
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+        legend.position=c(0.7, 0.95), legend.title=element_blank()) + 
+  labs(title="98% posterior interval of forecast within the timeframe 
+       (2019-09-17 ~ 2019-09-23) after training on 150 hours", 
+       x="Date", y="Return")
+
